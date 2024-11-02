45bd3b56175d2ffdda9e629d8984a6e4747ce2d9
WilliamCubbage <Cubbagwm@mail.uc.edu>
2024-11-02 12:00:02 -0400

    Delete Weather and add Name controller

---
+++ Controllers/NameController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourNamespace.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NameController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetName()
+        {
+            var result = new { Name = "Your Name" }; // Replace "Your Name" with your actual name
+            return Ok(result);
+        }
+    }
+}
